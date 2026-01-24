3565d6e40ee950fe2394e5bc84fb20617664112a
Anshul Batra <anshubatra2563@gmail.com>
2026-01-24 17:19:39 +0530

    added basic types for ToolButton

---
+++ apps/web/components/Toolbar/types.ts
@@ -0,0 +1,13 @@
+export type ToolType = "select" | "rectangle" | "ellipse" | "line" | "diamond-box";
+
+export type Tool = {
+    id : ToolType,
+    label : string,
+    icon : React.ReactNode
+}
+
+export type ToolButtonProps = {
+    tool : Tool,
+    onClick : () => void,
+    isActive : boolean
+}
